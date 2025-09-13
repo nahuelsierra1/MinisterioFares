36911a651880eacac4b598d82f80427166b7a3b8
nahuelsierra1 <nahuel_sierra2@hotmail.com>
2025-09-13 17:36:48 -0300

    conocenos terminado

--- ministerio-fares/src/assets/components/Conocenos.tsx
+++ ministerio-fares/src/assets/components/Conocenos.tsx
@@ -1,8 +1,9 @@
 function Conocenos() {
     return (
-        <section className="w-100 h-[1000px] flex flex-col bg-[url('./img/fondo-conocenos.jpg')] bg-cover bg-no-repeat">
-            <div className="text-center mt-20 mb-10">
-                <h3 className="text-[16px] font-bold text-green-600">QUIENES SOMOS?</h3>
+        <>
+        <section className="w-100 h-100vh flex flex-col bg-blue-700 px-5 text-white pb-20">
+            <div className="text-center mt-30 mb-10">
+                <h3 className="text-[16px] font-bold text-green-500">QUIENES SOMOS?</h3>
                 <h2 className="text-[45px] font-bold text-white leading-6">CONOCENOS</h2>
             </div>
             <div>
@@ -11,14 +12,17 @@ function Conocenos() {
                     la fe de las personas, guiándolas en su camino espiritual mediante canciones que 
                     reflejan la gracia y el poder transformador de Dios.</p>
             </div>
-            <div>
-                <h3>NUESTRA VISION</h3>
+            <div className="mt-14">
+                <h3 className="font-bold">NUESTRA VISION</h3>
                 <p>Ser una iglesia que impacte nuestra ciudad y mas alla, formando discipulos 
                     de Jesus que vivan en amor, fe y servicio. Queremos ser una familia espiritual 
                     donde cada persona encuentro proposito, sea transformada por el Evangelio y 
                     lleve el mesnaje de Cristo a cada area de la sociedad</p>
             </div>
+        
         </section>
+        <div className="bg-[url('./img/img-conocenos.jpg')] w-100 h-[300px] bg-cover bg-center"></div>
+        </>
     )
 }
 export default Conocenos
--- ministerio-fares/src/assets/components/Hero.tsx
+++ ministerio-fares/src/assets/components/Hero.tsx
@@ -4,24 +4,26 @@ function Hero() {
 
   return (
     <>
-
-    <section className="pl-7 pr-7 flex flex-col justify-center items-center h-[800px] w-full hero">
+    <section className="flex flex-col justify-center items-center h-svh w-full hero px-5">
       <Header></Header>
-      <div className=" text-white text-center mb-15">
+
+      <div className=" text-white text-center mb-15 z-10">
         <h3 className="text-xl font-bold m-0 leading-0">MINISTERIO</h3>
         <h2 className="text-8xl font-bold m-1">FARES</h2>
         <h3 className="text-xl font-bold m-0 leading-0">APOSTOLICO Y PROFETICO</h3>
       </div>
-      <div className="titulo">
+      <div className="titulo z-10">
         <h1 className="text-[80px] font-black text-white w-[90%] leading-[65px]">UN LUGAR PARA LA FAMILIA</h1>
       </div>
-      <hr className="border-0 w-[100%] h-1 bg-white my-[30px]"/>
-      <div className="reuniones text-white font-bold text-center">
+      <hr className="border-0 w-[100%] h-1 bg-white my-[30px] z-10"/>
+      <div className="reuniones text-white font-bold text-center z-10">
         <h3 className="mb-4 font-bold">REUNIONES</h3>
         <p className="border border-3 py-2 px-5 mb-2">MIERCOLES 19PM</p>
         <p className="border border-3 py-2 px-5">DOMINGOS 18PM</p>
       </div>
+      
     </section>
+          <div className="absolute bottom-0 w-full h-[600px] bg-gradient-to-t from-blue-700 via-blue-500 to-transparent z-0"></div>
     </>
   );
 }
